a539a3600bef0e2f1aa4f5dc8ef37a280d6e238d
PhOberd <phoberdorfer@edu.aau.at>
2022-03-13 19:23:39 +0100

    added calculation

--- app/src/main/java/com/example/einzelbeispiel_se2/Controller.java
+++
@@ -1,2 +0,0 @@
-package com.example.einzelbeispiel_se2;public class Controller {
-}
---
+++ app/src/main/java/com/example/einzelbeispiel_se2/ControllerServerCon.java
@@ -0,0 +1,29 @@
+package com.example.einzelbeispiel_se2;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+
+public class Controller extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView matNr=findViewById(R.id.matNr);
+    private Button btn=findViewById(R.id.button2);
+    private TextView serverAns=findViewById(R.id.serverAnswer);
+    private TCPClient client=new TCPClient();
+
+    @Override
+    public void onClick(View view) {
+        String matrNr = matNr.getText().toString();
+        try {
+            serverAns.setText(client.sendRequest(matrNr));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
